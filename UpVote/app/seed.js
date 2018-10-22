a3e86e9c2a930c0d404209757b550681ae73452c
Xavier <43478582+coder-log@users.noreply.github.com>
2018-10-22 08:13:21 +0200

    Submission section

---
+++ UpVote/app/seed.js
@@ -0,0 +1,15 @@
+
+{
+    "submissions": [
+        {
+        "id": 4 ,
+        "title": 5 ,
+        "url": "www.example.com",
+        "submissionImage": ./public/images/avatars/daniel.jpg ,
+        "avatar": ,
+        "description": "En un lugar de La Mancha, de cuyo nombre no quiero acordarme" ,
+
+        }
+
+    ]
+}
